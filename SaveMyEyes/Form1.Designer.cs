837b9e432547ef63d21f1cfbb3b3eb5479274e23
xordux <xordux@gmail.com>
2014-04-07 18:11:24 +0530

    First Commit
    This is a basic version and many of the intended functionalities are not
    working. Will work on them soon.

---
+++ SaveMyEyes/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SaveMyEyes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time between breaks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Break Duration";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Save My Eyes";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 185);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Save My Eyes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ SaveMyEyes/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SaveMyEyes
+{
+    public partial class Form1 : Form
+    {
+        public int WM_SYSCOMMAND = 0x0112;
+        public int SC_MONITORPOWER = 0xF170;
+        Timer timer = new Timer() ;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static class MonitorHelper
+        {
+            [DllImport("user32.dll")]
+            public static extern int SendMessage(int hWnd, int Msg, int wParam, int lParam);
+
+            public static void TurnOn()
+            {
+                SendMessage(-1, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_ON);
+            }
+
+            public static void TurnOff()
+            {
+                SendMessage(-1, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);
+            }
+
+            const int SC_MONITORPOWER = 0xF170;
+            const int WM_SYSCOMMAND = 0x0112;
+            const int MONITOR_ON = -1;
+            const int MONITOR_OFF = 2;
+        }
+
+        int interval;
+        public void FormWithTimer()
+        {
+            timer = new Timer();
+            timer.Tick += new EventHandler(turnOff); // Everytime timer ticks, turnOff will be called
+            
+            timer.Interval = (1000) * interval * 60;     // Timer will tick evert 'interval' minutes
+            timer.Enabled = true;                       // Enable the timer
+            timer.Start();                              // Start the timer
+        }
+
+        private void turnOff(object sender, EventArgs e)
+        {
+            timer.Stop();
+            MonitorHelper.TurnOff();
+            int result; //result is duration of break
+            result = (int.TryParse(textBox2.Text, out result)) ? result : 20;
+            textBox2.Text = result.ToString();
+
+            while (result > 0)
+            {
+                MonitorHelper.TurnOff();
+                System.Threading.Thread.Sleep(1000);
+                result--;
+            }
+            MonitorHelper.TurnOn();
+            timer.Start(); 
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result;
+            interval = 20;
+            interval = (int.TryParse(textBox1.Text, out result)) ? result : 20;
+            textBox1.Text = interval.ToString();
+            
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                button1.Text = "Start";
+            }
+            else
+            {
+                button1.Text = "Stop";
+                FormWithTimer();
+            }
+        }
+
+        // This code is for starting application in tray , (see Program.cs to fully understand this code)
+        
+    }
+}
